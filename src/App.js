a1649eaba463164b714a95bc833d351aec380515
rephanfrancis <ginika.chinonso.francis@gmail.com>
2022-12-09 14:31:08 +0100

    refactor: Adding routes to the root component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Addons from './components/Addons';
+import Profileinfo from './components/Profileinfo';
+import Selectplan from './components/Selectplan';
+import Sidebar from './components/Navbar';
+import Summary from './components/Summary';
+import Thankyou from './components/Thankyou';
+
 
 function App() {
+
+  const [user, setUser] = useState({})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col md:flex-row'>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path='/register/profile' element={< Profileinfo user={user} setUser={setUser}/>}/>
+          <Route path='/register/plan' element={< Selectplan />}/>
+          <Route path='/register/addons' element={< Addons />}/>
+          <Route path='/register/summary' element={< Summary />}/>
+          <Route path='/register/thankyou' element={< Thankyou />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
